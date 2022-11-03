fd3a5956cf0eb6d620c1f0e9e97eba98e6e846c2
seacat271 <seacat271083@gmail.com>
2022-11-03 22:04:05 +0200

    add placeholder clean double

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,25 +1,18 @@
-// import $ from 'jquery';
+
 $('.reviews__slider').slick({
   infinite: true,
   arrows: false,
   slidesToShow: 3,
   slidesToScroll: 1,
-  //   focusOnSelect: true,
   centerMode: true,
-  //   centerPadding: '0px',
   variableWidth: true,
   dots: false,
   mobileFirst: true,
-  //   adaptiveHeight: true,
   asNavFor: '.reviews__description',
 
   responsive: [
     {
-      //   breakpoint: 1280,
-      //   settings: {
-      //     slidesToShow: 3,
-      //     slidesToScroll: 1,
-      //   },
+
     },
     {
       breakpoint: 768,
@@ -45,17 +38,3 @@ $('.reviews__description').slick({
   arrows: false,
   infinite: true,
 });
-// $('.reviews__slider').on('beforeChange', (e, slick, currSlide, nextSlide) => {
-//   if (currSlide === nextSlide) {
-//     return;
-//   }
-//   $('[data-reviews]').addClass('big');
-//   $(`[data-slide]`).each((_, slideEl) => {
-//     const $slideEl = $(slideEl);
-//     console.log($slideEl);
-//     const currentSlide = $slideEl.attr('data-slide');
-//     $slideEl.toggleClass('big', +currentSlide === nextSlide);
-//     $('[data-reviews]').removeClass('big');
-//   });
-//   //   currSlide.addClass('big');
-// });
